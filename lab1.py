ac2105d830e97048faae74561a4e60e1821de9fc
Diky Ramadhani <dikyramadhani16@gmail.com>
2020-11-02 22:40:43 +0700

    update file readme.md

--- lab1.py
+++ lab1.py
@@ -1,1 +1,16 @@
-print("DIKY COK")
+#penggunaan end
+print('A', end='')
+print('B', end='')
+print('C', end='')
+print()
+print('X')
+print('Y')
+print('z')
+
+#penggunaan separator
+w, x, y, z = 10, 15, 20, 25
+print(w, x, y, z)
+print(w, x, y, z, sep=',')
+print(w, x, y, z, sep='')
+print(w, x, y, z, sep=':')
+print(w, x, y, z, sep='-----')
